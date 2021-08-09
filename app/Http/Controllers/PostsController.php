5857328a875199d75d0d91494dd5b1312f5820cc
Sungbin Lee <sunglee@addu.edu.ph>
2021-08-09 22:52:40 +0900

    Creating $ storing a post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PostsController extends Controller
 {
@@ -24,7 +25,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
@@ -35,7 +36,27 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,png,jpeg|max:5048'
+        ]);
+
+        $newImageName = uniqid() . '-' . $request->title . '.' . $request->image->extension();
+
+        $request->image->move(public_path('image'), $newImageName);
+
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+
+        Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'slug' => SlugService::createSlug(Post::class, 'slug', $request->title),
+            'image_path' => $newImageName,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect('/blog')->with('message', 'Your post has been added!');
     }
 
     /**
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,6 +9,24 @@
         </div>
     </div>
 
+    @if(session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2">
+            <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-3xl">
+                {{ session()->get('message') }}
+            </p>
+        </div>
+    @endif
+
+    @if(Auth::check())
+        <div class="pt=15 w-4/5 m-auto">
+            <a
+                href="/blog/create"
+                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                Create post
+            </a>
+        </div>
+    @endif
+
     @foreach($posts as $post)
 
         <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
